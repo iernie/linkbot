23aa3e1daaff759cfba8f8a615f11f2be910465f
ernie <ernie@epicbytes.net>
2013-05-20 23:13:00 +0200

    added date to logging

--- plugins/plugin.logging.js
+++ plugins/plugin.logging.js
@@ -1,9 +1,11 @@
 var db = require("mongojs").connect("nodebot", ["logs"]);
+var moment = require("moment");
 
 exports.init = function(client, from, to, message) {
     db.logs.save({
         'channel': to,
         'log': {
+        	'date': moment().format("YYYY/MM/DD HH:mm:ss"),
             'nick': from,
             'message': message
         }
