a24b70e5ba02ea06ff645cf59e2716e478890c2e
ernie <ernie@epicbytes.net>
2016-06-12 01:45:05 +0200

    Added tell plugin

---
+++ plugins/plugin.tell.js
@@ -0,0 +1,28 @@
+module.exports = (client) => {
+  const reminders = [];
+
+  client.addListener('message', (from, to, message) => {
+    if (message.match(/^!tell/)) {
+      const matches = message.match(/^(\S+)\s(\S+)\s(.*)/);
+      if (matches !== null) {
+        const query = matches.splice(1);
+        if (query !== null && query.length > 0 && query[1] !== null && query[1].trim() !== '' && query[2] !== null && query[2].trim() !== '') {
+          reminders.push({
+            from,
+            to: query[1].trim(),
+            message: query[2].trim()
+          });
+        }
+      }
+    }
+  });
+
+  client.addListener('join', (from, to) => {
+    reminders.forEach((reminder, index, array) => {
+      if (reminder.to === to) {
+        client.say(from, `${reminder.from}: ${reminder.message}`);
+        array.splice(index, 1);
+      }
+    });
+  });
+};
