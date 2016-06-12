14d615df14743389891e5ce0c3e57ccdee837996
ernie <ernie@epicbytes.net>
2016-06-12 12:25:50 +0200

    cleanup

--- plugins/plugin.hemen.js
+++ plugins/plugin.hemen.js
@@ -14,19 +14,16 @@ function parseBodyFunction(client, to) {
 
 module.exports = (client) => {
   client.addListener('message', (from, to, message) => {
-    const parseBody = parseBodyFunction(client, to);
     if (message.match(/^!hemen/)) {
+      let queryParam = '';
       const matches = message.match(/^(\S+)\s(.*)/);
       if (matches !== null) {
         const query = matches.splice(1);
         if (query !== null && query.length > 0 && query[1] !== null && query[1].trim() !== '') {
-          request(`http://hemmis.444.no/hemensier/search?q=${query[1].trim()}`, parseBody);
-        } else {
-          request('http://hemmis.444.no/hemensier/', parseBody);
+          queryParam = `search?q=${query[1].trim()}`;
         }
-      } else {
-        request('http://hemmis.444.no/hemensier/', parseBody);
       }
+      request(`http://hemmis.444.no/hemensier/${queryParam}`, parseBodyFunction(client, to));
     }
   });
 };
