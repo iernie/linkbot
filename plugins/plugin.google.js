1b65295ec670440e85008cf94dff98a1901980ef
ernie <ernie@epicbytes.net>
2016-06-13 11:33:55 +0200

    google plugin

---
+++ plugins/plugin.google.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+function parseBodyFunction(client, to) {
+  return (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const contentType = response.headers['content-type'];
+      if (contentType.split(';')[0] === 'application/json') {
+        const json = JSON.parse(body);
+        if (json.items !== null && json.items.length > 0) {
+          client.say(to, `>> ${json.items[0].title} ${json.items[0].link}`);
+        }
+      }
+    }
+  };
+}
+
+module.exports = (client, plugin) => {
+  client.addListener('message', (from, to, message) => {
+    if (message.match(/^!g/)) {
+      const matches = message.match(/^(\S+)\s(.*)/);
+      if (matches !== null) {
+        const query = matches.splice(1);
+        if (query !== null && query.length > 0 && query[1] !== null && query[1].trim() !== '') {
+          request(`https://www.googleapis.com/customsearch/v1?key=${plugin.options.key}&cx=${plugin.options.cx}&q=${query[1].trim()}`, parseBodyFunction(client, to));
+        }
+      }
+    }
+  });
+};
