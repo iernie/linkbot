34d096c5b92de3101440faae05018f2d6e3bce5f
ernie <ernie@epicbytes.net>
2016-06-19 01:13:21 +0200

    fix broken regex

--- plugins/plugin.natta.js
+++ plugins/plugin.natta.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   client.addListener('message', (from, to, message) => {
-    if (message.match(new RegExp(`^(natta|god|natt|,| )+(!|${client.nick}|alle|sammen|,| )*`, 'ig'))) {
+    if (message.match(new RegExp(`^((god ?)?natta?)(!|${client.nick}|alle|sammen|,| )*$`, 'ig'))) {
       client.say(to, `natta ${from}!`);
     }
   });
