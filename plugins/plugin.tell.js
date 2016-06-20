c9b9a90c0e1389299162f9b9e018e5d35e59162f
ernie <ernie@epicbytes.net>
2016-06-20 13:14:21 +0200

    Tell people that are idling

--- plugins/plugin.tell.js
+++ plugins/plugin.tell.js
@@ -1,8 +1,8 @@
-const _ = require('lodash');
 const jsonfile = require('jsonfile');
 const file = './reminders.json';
 
 module.exports = (client) => {
+  let shouldSave = false;
   let reminders = [];
   jsonfile.readFile(file, (err, obj) => {
     if (!err) {
@@ -10,44 +10,43 @@ module.exports = (client) => {
     }
   });
 
-  client.addListener('message', (from, to, message) => {
-    if (message.match(/^!tell /)) {
-      const matches = message.match(/^(\S+)\s([a-z0-9æøå_]+)\s(.*)/i);
-      if (matches !== null) {
-        const query = matches.splice(1);
-        if (query !== null && query.length > 0 && query[1] !== null && query[1].trim() !== '' && query[2] !== null && query[2].trim() !== '') {
-          const user = query[1].trim().toLowerCase();
-          const users = _.map(_.keys(client.chans[to].users), u => u.toLowerCase());
-          if (!_.isEmpty(_.filter(users, u => u === user))) {
-            if (user === client.nick.toLowerCase()) {
-              client.say(to, 'Tulling, jeg er jo her!');
-            } else {
-              client.say(to, `Tulling, ${query[1].trim()} er jo her!`);
-            }
-            return;
-          }
-
-          reminders.push({
-            from,
-            channel: to,
-            to: query[1].trim(),
-            message: query[2].trim()
-          });
-          jsonfile.writeFileSync(file, reminders);
-          client.say(to, `${from}, notert!`);
-        }
-      }
-    }
-  });
-
-  client.addListener('join', (from, to) => {
+  function checkMessages(channel, to) {
     reminders = reminders.filter((reminder) => {
-      if (to.match(new RegExp(`${reminder.to}`, 'ig')) && reminder.channel === from) {
-        client.say(from, `${to}: ${reminder.message} (mvh ${reminder.from})`);
+      if (to.match(new RegExp(`${reminder.to}`, 'ig')) && reminder.channel === channel) {
+        client.say(channel, `${to}: ${reminder.message} (mvh ${reminder.from})`);
+        shouldSave = true;
         return false;
       }
       return true;
     });
-    jsonfile.writeFileSync(file, reminders);
+    if (shouldSave) {
+      shouldSave = false;
+      jsonfile.writeFileSync(file, reminders);
+    }
+  }
+
+  client.addListener('message', (from, to, message) => {
+    const matches = message.match(/^!tell ([a-z0-9æøå_]+)\s(.+)/i);
+    if (matches !== null) {
+      if (matches[1].trim().toLowerCase() === client.nick.toLowerCase()) {
+        client.say(to, 'Tulling, jeg er jo her!');
+        return;
+      }
+
+      reminders.push({
+        from,
+        channel: to,
+        to: matches[1].trim(),
+        message: matches[2].trim()
+      });
+      jsonfile.writeFileSync(file, reminders);
+      client.say(to, `${from}, notert!`);
+    } else {
+      checkMessages(to, from);
+    }
+  });
+
+  client.addListener('join', (from, to) => {
+    checkMessages(from, to);
   });
 };
