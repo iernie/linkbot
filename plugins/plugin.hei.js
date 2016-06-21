0b0d3912f4d0dc61ff61075d17f9ea46913b72b2
ernie <ernie@epicbytes.net>
2016-06-21 12:14:17 +0200

    new regex

--- plugins/plugin.hei.js
+++ plugins/plugin.hei.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   client.addListener('message', (from, to, message) => {
-    if (message.match(new RegExp(`^((god )?(hei|hallo|heisann|morn|morgen|kveld)+)( ${client.nick})?$`, 'i'))) {
+    if (message.match(new RegExp(`^${client.nick}`, 'i'))) {
       client.say(to, `hei på deg ${from} :)`);
     }
   });
