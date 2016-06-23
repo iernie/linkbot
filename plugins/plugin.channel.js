459cdeb69e15d992559768b71e015f5f8972545d
ernie <ernie@epicbytes.net>
2016-06-23 16:13:50 +0200

    channel

---
+++ plugins/plugin.channel.js
@@ -0,0 +1,9 @@
+module.exports = (client) => {
+  client.addListener('message', (from, to, message) => {
+    const matches = message.match(/^!channel (\S.*)/i);
+    if (matches !== null) {
+      const users = _.filter(_.keys(client.chans[to].users), u => u.toLowerCase() !== from.toLowerCase()).join(', ');
+      client.say(to, `${users}: ${matches[1]} (mvh ${from})`);
+    }
+  });
+};
