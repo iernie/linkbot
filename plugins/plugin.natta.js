ae4f93d4468788b5f395266348d1032e69624fee
ernie <ernie@epicbytes.net>
2016-07-03 23:34:57 +0200

    natta

--- plugins/plugin.natta.js
+++ plugins/plugin.natta.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 module.exports = (client) => {
   const users = new Map();
   client.addListener('message', (from, to, message) => {
-    if (message.match(new RegExp(`((go+d ?)?nat+a?)(!|${client.nick}|alle|sammen|,| )*$`, 'i'))) {
+    if (message.match(new RegExp(`^.{0,5}((go+d ?)?nat+a?)(!|${client.nick}|alle|sammen|,| )*$`, 'i'))) {
       const user = users.get(from.toLowerCase());
       if (user === undefined || moment().diff(user.time, 'minutes') >= 5) {
         client.say(to, `natta ${from}!`);
