61d335cbb2e168affc86cf93b8e62be08b05c63b
ernie <ernie@epicbytes.net>
2017-01-18 13:18:46 +0100

    air

---
+++ plugins/plugin.air.js
@@ -0,0 +1,32 @@
+const luftkvalitet = require('luftkvalitet');
+const c = require('irc-colors');
+
+module.exports = (client, say) => {
+  client.addListener('message', (from, to, message) => {
+    const matches = message.match(/^!air (\S.*)/i);
+    if (matches !== null) {
+      const location = matches[1].trim();
+      luftkvalitet({type: 'Country'}, (error, array) => {
+        if (!error) {
+          array
+            .filter(x => x.Name.toLowerCase() === location.toLowerCase())
+            .forEach((data) => {
+              let text;
+              switch(data.Text) {
+                case 'Lite':
+                  text = c.green(data.ShortDescription);
+                  break;
+                case 'Moderat':
+                  text = c.brown(data.ShortDescription);
+                  break;
+                default:
+                  text = c.red(data.ShortDescription);
+                  break;
+              }
+              say(to, `${data.Name}: ${text}`);
+            });
+        }
+      });
+    }
+  });
+};
