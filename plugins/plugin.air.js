9a9f0edbccc4064ad120e9d364cadcea240b9ad8
ernie <ernie@epicbytes.net>
2017-01-18 13:26:27 +0100

    more colors

--- plugins/plugin.air.js
+++ plugins/plugin.air.js
@@ -6,21 +6,27 @@ module.exports = (client, say) => {
     const matches = message.match(/^!air (\S.*)/i);
     if (matches !== null) {
       const location = matches[1].trim();
-      luftkvalitet({type: 'Country'}, (error, array) => {
+      luftkvalitet({ type: 'Country' }, (error, array) => {
         if (!error) {
           array
             .filter(x => x.Name.toLowerCase() === location.toLowerCase())
             .forEach((data) => {
               let text;
-              switch(data.Text) {
+              switch (data.Text) {
                 case 'Lite':
                   text = c.green(data.ShortDescription);
                   break;
                 case 'Moderat':
                   text = c.brown(data.ShortDescription);
                   break;
-                default:
+                case 'Høy':
                   text = c.red(data.ShortDescription);
+                  break;
+                case 'Svært høy':
+                  text = c.purple(data.ShortDescription);
+                  break;
+                default:
+                  text = data.ShortDescription;
                   break;
               }
               say(to, `${data.Name}: ${text}`);
