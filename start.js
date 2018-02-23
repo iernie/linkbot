a244db5892206037028976af7e547990ce25ed38
ernie <ernie@epicbytes.net>
2018-02-23 14:28:41 +0100

    parse

--- start.js
+++ start.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
+const Parse = require('parse/node');
 
 const client = new Discord.Client();
+Parse.initialize(process.env.parse_app_id);
+Parse.serverURL = process.env.parse_server_url;
 
 
 client.login(process.env.token);
