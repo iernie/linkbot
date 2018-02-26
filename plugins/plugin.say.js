6bf75fa4ce9412e40fdea8d1b691c84b85d6fdd2
ernie <ernie@epicbytes.net>
2018-02-26 12:57:14 +0100

    fiks say

--- plugins/plugin.say.js
+++ plugins/plugin.say.js
@@ -2,10 +2,10 @@ module.exports = (client) => {
   client.on('message', (message) => {
     if (message.author.bot || message.channel.type !== 'dm') return;
 
-    const matches = message.content.match(/!say #(\S+)\s(.+)/i);
+    const matches = message.content.match(/!say #?(\S+)\s(.+)/i);
     if (matches) {
       const channel = client.channels.find('name', matches[1]);
-      channel.say(matches[2]);
+      channel.send(matches[2]);
     }
   });
 };
