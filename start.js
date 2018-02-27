9d04fba68ecb141e9e6402030dcb434661b0ad64
ernie <ernie@epicbytes.net>
2018-02-27 08:36:01 +0100

    ready

--- start.js
+++ start.js
@@ -5,6 +5,10 @@ const client = new Discord.Client();
 Parse.initialize(process.env.parse_app_id);
 Parse.serverURL = process.env.parse_server_url;
 
+client.on('ready', () => {
+  console.log('Bip bop, I am ready!');
+});
+
 require(`./plugins/plugin.8ball`)(client); // eslint-disable-line
 require(`./plugins/plugin.air`)(client); // eslint-disable-line
 require(`./plugins/plugin.hei`)(client); // eslint-disable-line
