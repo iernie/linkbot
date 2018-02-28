e0e164ea4f5925f2687032c829363d075136d778
ernie <ernie@epicbytes.net>
2018-02-28 13:36:11 +0100

    fix

--- plugins/plugin.old.js
+++ plugins/plugin.old.js
@@ -16,43 +16,51 @@ module.exports = (client) => {
   client.on('message', (message) => {
     if (message.author.bot) return;
 
-    const queryTemp = new Parse.Query(URL);
-    queryTemp.limit(1000);
-    queryTemp.find().then((results) => {
-      if (results && results.length) {
-        results.forEach((result) => {
-          const url = urlParser.parse(normalizeUrl(result.get('url'), { normalizeHttps: true, removeDirectoryIndex: true }));
-          if (url.path && url.path !== '/') {
-            result.set('url', url.href);
-            result.save();
-          } else {
-            result.destory();
-          }
-        });
-        console.log('done');
-      }
-    });
+    if (message.content === 'test') {
+      console.log('starting');
+      const queryTemp = new Parse.Query(URL);
+      queryTemp.limit(1000);
+      queryTemp.find().then((results) => {
+        console.log('got results');
+        if (results && results.length) {
+          results.forEach((result) => {
+            const url = urlParser.parse(normalizeUrl(result.get('url'), { normalizeHttps: true, removeDirectoryIndex: true }));
+            if (url.path && url.path !== '/') {
+              result.set('url', url.href);
+              result.save();
+            } else {
+              result.destory();
+            }
+          });
+          console.log('done');
+        }
+      });
+    }
 
     const matches = message.content.match(pattern);
     if (matches) {
       matches
         .map(url => urlParser.parse(normalizeUrl(url, { normalizeHttps: true, removeDirectoryIndex: true })))
         .forEach(async (url) => {
-          const query = new Parse.Query(URL);
-          query.equalTo('url', url.href);
-          query.equalTo('channel', message.channel.id);
-          const result = await query.first();
-          if (result) {
-            if (result.get('user') !== message.author.id) {
-              const days = distanceInWordsToNow(result.get('createdAt'), { includeSeconds: true, locale: nb });
-              message.reply(`old! Denne lenken ble postet av <@${result.get('user')}> for ${days} siden.`);
+          try {
+            const query = new Parse.Query(URL);
+            query.equalTo('url', url.href);
+            query.equalTo('channel', message.channel.id);
+            const result = await query.first();
+            if (result) {
+              if (result.get('user') !== message.author.id) {
+                const days = distanceInWordsToNow(result.get('createdAt'), { includeSeconds: true, locale: nb });
+                message.reply(`old! Denne lenken ble postet av <@${result.get('user')}> for ${days} siden.`);
+              }
+            } else if (url.path && url.path !== '/') {
+              const urlObject = new URL();
+              urlObject.set('url', url.href);
+              urlObject.set('user', message.author.id);
+              urlObject.set('channel', message.channel.id);
+              urlObject.save();
             }
-          } else if (url.path && url.path !== '/') {
-            const urlObject = new URL();
-            urlObject.set('url', url.href);
-            urlObject.set('user', message.author.id);
-            urlObject.set('channel', message.channel.id);
-            urlObject.save();
+          } catch (err) {
+            console.log(err);
           }
         });
     }
