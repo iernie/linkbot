6cd6a87f34775cd4d2b51709067ddd68808e5fab
ernie <ernie@epicbytes.net>
2018-03-01 11:15:11 +0100

    react

--- plugins/plugin.react.js
+++ plugins/plugin.react.js
@@ -2,9 +2,43 @@ module.exports = (client) => {
   client.on('message', (message) => {
     if (message.author.bot) return;
 
-    if (message.content.match(/test/i)) {
-      console.log(client.emojis.map(e => e.toString()).join(' '));
-      const emoji = client.emojis.find('name', 'large_blue_circle');
+    if (message.content.match(/aire|sjura/i)) {
+      const emoji = client.emojis.find('name', 'aire');
+      if (emoji) {
+        message.react(emoji.id);
+      }
+    }
+
+    if (message.content.match(/lasse?/i)) {
+      const emoji = client.emojis.find('name', 'lass_ape');
+      if (emoji) {
+        message.react(emoji.id);
+      }
+    }
+
+    if (message.content.match(/hemen/i)) {
+      const emoji = client.emojis.find('name', 'hemen');
+      if (emoji) {
+        message.react(emoji.id);
+      }
+    }
+
+    if (message.content.match(/redmist/i)) {
+      const emoji = client.emojis.find('name', 'redmist');
+      if (emoji) {
+        message.react(emoji.id);
+      }
+    }
+
+    if (message.content.match(/klausulen/i)) {
+      const emoji = client.emojis.find('name', 'klausulen');
+      if (emoji) {
+        message.react(emoji.id);
+      }
+    }
+
+    if (message.content.match(/ponas/i)) {
+      const emoji = client.emojis.find('name', 'ponas');
       if (emoji) {
         message.react(emoji.id);
       }
