a5e4e0bd91442d0be11265c8e701e44c513c20f5
einar afiouni <einar.afiouni@bekk.no>
2018-04-17 14:59:35 +0200

    8ball

--- plugins/plugin.8ball.js
+++ plugins/plugin.8ball.js
@@ -1,13 +1,13 @@
 module.exports = (client) => {
   client.on('message', (message) => {
-    if (message.author.bot) return;
-
     const matches = message.content.match(/^!8ball (\S.*(\|| or | eller ).+)/i);
     if (matches) {
       const queries = matches[1].split(/\|| or | eller /i).map(val => val.replace(/\?$/, '').trim());
       const random = Math.floor(Math.random() * queries.length);
       message.channel.send(queries[random]);
     }
+
+    if (message.author.bot) return;
 
     if (message.content.match(/^!help/i)) {
       message.channel.send('!8ball [Q1] (eller, or, |) [Q2] [...Qx]');
