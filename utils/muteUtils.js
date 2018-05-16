8fb7b7ccd641c18b0ad0be2f3b4ea093f26a2586
einar afiouni <einar.afiouni@bekk.no>
2018-05-16 11:15:36 +0200

    hmm

--- utils/muteUtils.js
+++ utils/muteUtils.js
@@ -1,6 +1,6 @@
 const Mute = Parse.Object.extend('Mute');
 
-module.exports = async (channelId) => {
+module.exports = (channelId) => {
   const mute = new Parse.Query(Mute);
   mute.equalTo('channel', channelId);
   return mute.first();
