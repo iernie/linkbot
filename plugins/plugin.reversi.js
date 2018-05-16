40786243a60b84f0261cc29b4365f54ae212cbe1
einar afiouni <einar.afiouni@bekk.no>
2018-05-16 16:06:59 +0200

    reversi--

--- plugins/plugin.reversi.js
+++ plugins/plugin.reversi.js
@@ -1,20 +1,6 @@
 const { Game } = require('reversi');
 
-let games = [];
-
-const removeGame = (channelId) => {
-  const index = games.findIndex(g => g[0] === channelId);
-  if (index !== -1) {
-    games = games.splice(index, 1);
-  }
-};
-
-const addGame = (channnelId) => {
-  removeGame(channnelId);
-  const game = new Game();
-  games.push([channnelId, game]);
-  return game;
-};
+let game = null;
 
 module.exports = (client) => {
   client.on('message', (message) => {
@@ -23,16 +9,14 @@ module.exports = (client) => {
     const matches = message.content.match(/^!(othello|reversi) (((\d),(\d))|new)/i);
     if (matches) {
       if (matches[2] === 'new') {
-        const game = addGame(message.channel.id);
+        game = new Game();
         message.channel.send(game.toText());
       } else if (matches[3]) {
-        const index = games.findIndex(g => g[0] === message.channel.id);
-        if (index !== -1) {
-          const game = games[index][1];
+        if (game) {
           game.proceed(matches[4], matches[5]);
           message.channel.send(game.toText());
           if (game.isEnded) {
-            removeGame(message.channel.id);
+            game = null;
             message.channel.send('Game over!');
           }
         }
