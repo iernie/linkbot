1b0091d4ff27612eef4672893542c27b3620a2a9
einar afiouni <einar.afiouni@bekk.no>
2018-05-16 10:59:28 +0200

    unmute

--- plugins/plugin.mute.js
+++ plugins/plugin.mute.js
@@ -1,16 +1,24 @@
 const Mute = Parse.Object.extend('Mute');
 
 module.exports = (client) => {
-  client.on('message', (message) => {
+  client.on('message', async (message) => {
     if (message.author.bot) return;
 
     const matches = message.content.match(/!mute #?(\S+)/i);
     if (matches) {
       const channel = client.channels.find('name', matches[1]);
+
       if (channel) {
-        const muteObject = new Mute();
-        muteObject.set('channel', channel.id);
-        muteObject.save();
+        const query = new Parse.Query(Mute);
+        query.equalTo('channel', channel.id);
+        const result = await query.first();
+        if (result) {
+          result.destroy();
+        } else {
+          const muteObject = new Mute();
+          muteObject.set('channel', channel.id);
+          muteObject.save();
+        }
       }
     }
   });
