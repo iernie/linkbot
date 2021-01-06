ddda2b5dcfd3281033accba0e5d521b90e907f6e
einar afiouni <einar.afiouni@bekk.no>
2021-01-06 17:47:14 +0100

    fix new year

--- plugins/plugin.newyear.js
+++ plugins/plugin.newyear.js
@@ -1,16 +1,16 @@
-const differenceInCalendarDays = require('date-fns/differenceInCalendarDays');
+const formatDistanceToNow = require('date-fns/formatDistanceToNow');
+const nb = require('date-fns/locale/nb');
 
 module.exports = (client) => {
   client.on('message', (message) => {
     if (message.author.bot) return;
 
-    const match = message.content.match(new RegExp('^((godt ?nytt ?år)|(happy ?new ?year))(!+)?$', 'i'));
+    const match = message.content.match(new RegExp('^.{0,5}((godt ?nytt ?år)|(happy ?new ?year))(!+)?$', 'i'));
     if (match && match[0] !== '') {
-      const days = differenceInCalendarDays(new Date(new Date().getFullYear(), 11, 31), new Date());
-      if (days === 0 || days === 1 || days === 364) {
+      if (new Date().getMonth() === 0) {
         message.channel.send(`Godt nytt år <@${message.author.id}>!`);
       } else {
-        message.channel.send(`Du er ${days} dager for tidlig!`);
+        message.channel.send(`Du er for tidlig! Det er ${formatDistanceToNow(new Date(new Date().getFullYear() + 1, 0, 1, 0, 0, 0, 0), { includeSeconds: true, locale: nb })} igjen.`);
       }
     }
   });
