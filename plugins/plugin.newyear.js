245bd694369316cee73ecbf70e2b664cace2f5d9
einar afiouni <einar.afiouni@bekk.no>
2021-01-06 16:48:21 +0100

    fix regex

--- plugins/plugin.newyear.js
+++ plugins/plugin.newyear.js
@@ -4,7 +4,8 @@ module.exports = (client) => {
   client.on('message', (message) => {
     if (message.author.bot) return;
 
-    if (message.content.match(new RegExp('^(((go+dt ?)?nytt *år)|(happy *new *year))*(!*)?$', 'i'))) {
+    const match = message.content.match(new RegExp('^((godt ?nytt ?år)|(happy ?new ?year))(!+)?$', 'i'));
+    if (match && match[0] !== '') {
       const days = differenceInCalendarDays(new Date(new Date().getFullYear(), 11, 31), new Date());
       if (days === 0 || days === 1 || days === 364) {
         message.channel.send(`Godt nytt år <@${message.author.id}>!`);
