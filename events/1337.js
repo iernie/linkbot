c0d0d377e11b345a2aa49e0912f7f9b83e2acfff
ernie <ernie@epicbytes.net>
2023-07-01 19:51:13 +0200

    leet

--- events/1337.js
+++ events/1337.js
@@ -8,7 +8,7 @@ export default {
   name: Events.MessageCreate,
   async execute(message) {
     const time = new Date(message.createdTimestamp);
-    if (!message.author.bot && message.content.match(new RegExp("1337", "i"))) {
+    if (!message.author.bot && message.content.match(new RegExp("^1337$", "i"))) {
       if (getHours(time) === 13 && getMinutes(time) === 37) {
         const docRef = doc(db, message.guildId, "counters", "1337", message.author.id);
         const docSnap = await getDoc(docRef);
